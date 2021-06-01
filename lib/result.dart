e76808cd10c8d29182c851c6f945599742fe3521
sho <ikeguchi.sho@gmail.com>
2021-06-01 22:37:09 +0900

    Add: reset button for count

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
   String get resultPhrase {
     String resultText = 'You did it!';
     if (resultScore <= 8) {
@@ -21,9 +22,19 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: Text('Restart Question!'),
+            textColor: Colors.blue,
+          )
+        ],
       ),
     );
   }
